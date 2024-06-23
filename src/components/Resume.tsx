a067844bf103a47c1d7ecff179482d3cdaf88506
krokerdile <crazykroker@gmail.com>
2024-06-23 18:35:37 +0900

    feat: Home 페이지 추가, PDF 다운로드 버튼 추가
    - react-to-print를 활용하여 이력서 pdf로 다운 받을 수 있도록 적용

---
+++ src/components/Resume.tsx
@@ -0,0 +1,5 @@
+import ResumeComponent from './resume.mdx';
+
+export default function Resume() {
+  return <ResumeComponent className="text-left" />;
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import { ReactInstance, useRef } from 'react';
+import Resume from '../components/Resume';
+import { useReactToPrint } from 'react-to-print';
+
+export default function Home() {
+  const printRef = useRef<ReactInstance | null>(null);
+
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+  });
+
+  return (
+    <div className="w-full flex flex-col justify-center p-2">
+      <div className="w-[800px] flex flex-col justify-left">
+        <button
+          onClick={handlePrint}
+          className="mb-4 p-2 bg-blue-500 text-white max-w-[200px] h-15 rounded-md hover:bg-blue-600"
+        >
+          PDF로 다운로드
+        </button>
+        <div ref={printRef}>
+          <Resume />
+        </div>
+      </div>
+    </div>
+  );
+}
