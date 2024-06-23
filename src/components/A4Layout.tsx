0168b425d3d5b152b79ade73a5cfca346a71c498
krokerdile <crazykroker@gmail.com>
2024-06-23 18:34:23 +0900

    feat: A4 레이아웃 컴포넌트 추가

---
+++ src/components/A4Layout.tsx
@@ -0,0 +1,9 @@
+const A4Layout = ({ children }) => {
+  return (
+    <div className="w-[210mm] min-h-[297mm] mx-auto p-10 bg-white shadow-md box-border text-left flex flex-col justify-start text-sm">
+      {children}
+    </div>
+  );
+};
+
+export default A4Layout;
